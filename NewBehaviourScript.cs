8bd34cb2affcf725253315c070f18ced2dbbd51d
amany alattas <amanyalattass@gmail.com>
2018-10-21 11:16:57 +0300

    First upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public GameObject target;//the object we want to bring it
+    public GameObject targetgirl;
+    void boys()
+    {
+        Instantiate(target, new Vector3(-48.94593f, -4.2f, -22.01f), Quaternion.identity);
+//eturn 0;
+    }
+
+    void girls()
+    {
+        Instantiate(targetgirl, new Vector3(-48.94593f, -4.2f, -17.01f), Quaternion.identity);
+        //eturn 0;
+    }
+    // Use this for initialization
+    void Start () {
+
+        while (Time.realtimeSinceStartup != 11)
+        {
+            Invoke("boys", 1);
+            Invoke("girls", 3);
+        }
+      
+        //Invoke("boys",3);
+        //Invoke("boys",5);
+        //Invoke("girls", 1);
+        //Invoke("girls", 3);
+        //Invoke("girls", 5);
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+      //  Invoke("boys", );
+
+
+        Destroy(target, 3);
+	}
+}
---
+++ Timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public Text timerText;
+    private float start;
+
+	// Use this for initialization
+	void Start () {
+        start = Time.time;//scince time start
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      float t  =Time.time - start;
+        string min = ((int)t / 60).ToString();
+        string sec = (t % 60).ToString("f0");
+        timerText.text = min + " : " + sec;
+
+	}
+}
