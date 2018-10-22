d781951d3e08caa140048ff39686926fd1cca0be
amany alattas <amanyalattass@gmail.com>
2018-10-22 14:50:05 +0300

    change timer class

--- Timer.cs
+++ Timer.cs
@@ -2,23 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour {
-
-    public Text timerText;
+    [SerializeField]
+    // float timer = 10f;//scne timer
+    
+    public string loadScene;//this to load a scene after timer end
+    
+    public Text timerText;//display timer
     private float start;
 
 	// Use this for initialization
 	void Start () {
+        timerText = GetComponent<Text>();//get the text on the obj
         start = Time.time;//scince time start
 	}
 	
 	// Update is called once per frame
 	void Update () {
-      float t  =Time.time - start;
+      //  timer -= Time.deltaTime;
+       float t  =Time.time - start;
+       // timerText.text = timer.ToString("f0");
+        
         string min = ((int)t / 60).ToString();
         string sec = (t % 60).ToString("f0");
         timerText.text = min + " : " + sec;
+        int x = int.Parse(min);
 
-	}
+        if (x == 1)
+        {
+            SceneManager.LoadScene("EndScen");
+        }
+
+    }
 }
