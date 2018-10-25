3c39d6ec6954c60d709cd55c37907f42cf7911e2
amnahOmar <VRforAutism@gmail.com>
2018-10-25 09:53:32 +0300

    this script of claps student in the classroom

---
+++ clap.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clap : MonoBehaviour
+{
+
+    static Animator anime;
+    public float countdown = 1000.0f;
+
+
+    void Start()
+    {
+        anime = GetComponent<Animator>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+         if(OVRInput.RawButton("clap"){
+         anime.ResetTrigger("clap");
+         }
+
+         if(OVRInput.RawTouch("handRaising"){
+         anime.ResetTrigger("handRaising");
+         }
+
+         */
+
+
+        countdown -= Time.deltaTime;
+        if (countdown <= 0.0f)//the girl will clap after 10 seconds
+        {
+            anime.ResetTrigger("clap");
+        }
+        if(countdown == 5.0f)
+        {
+            anime.SetTrigger("handRaising");
+        }
+    }
+}
